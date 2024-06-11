d174a1b81f16518a8b057adc723cc5281065ec5c
SavvinPC <sss020205@yandex.ru>
2024-06-11 03:18:23 +0300

    deque with base operations was added here

--- deque/deque.cpp
+++ deque/deque.cpp
@@ -22,20 +22,31 @@ public:
 
     bool isEmpty();
     size_t getLength();
-    void pushBack(DynamicArray<T> data);
-    void pushFront(DynamicArray<T> data);
+    void pushBack(DynamicArray<T> element);
+    void pushFront(DynamicArray<T> element);
     DynamicArray<T> *popBack();
     DynamicArray<T> *popFront();
-
+    DynamicArray<T> *peekBack();
+    DynamicArray<T> *peekFront();
 };
 
 template<typename T>
-DynamicArray<T> *Deque<T>::popFront() {
+DynamicArray<T> *Deque<T>::peekFront() {
+    return this -> data -> getFirst();
+}
+
+template<typename T>
+DynamicArray<T> *Deque<T>::peekBack() {
+    return this -> data -> getLast();
+}
+
+template<typename T>
+DynamicArray<T> *Deque<T>::popBack() {
     if (this -> isEmpty()) {
         throw std::invalid_argument("Error! Queue is empty");
     }
 
-    T *element = this -> ();
+    DynamicArray<T> *element = this -> data -> getLast();
     auto *oldTail = this -> data -> tail;
     this -> data -> tail = this -> data -> tail -> pointerOnPrevElement;
     delete oldTail;
@@ -44,54 +55,35 @@ DynamicArray<T> *Deque<T>::popFront() {
 }
 
 template<typename T>
-DynamicArray<T> *Deque<T>::popBack() {
+DynamicArray<T> *Deque<T>::popFront() {
+    if (this -> isEmpty()) {
+        throw std::invalid_argument("Error! Queue is empty");
+    }
 
+    DynamicArray<T> *element = this -> data -> getFirst();
+    auto *oldHead = this -> data -> head;
+    this -> data -> head = this -> data -> head -> pointerOnNextElement;
+    delete oldHead;
+
+    return element;
 }
 
 template<typename T>
-void Deque<T>::pushFront(DynamicArray<T> data) {
-    auto *newNode = new Node<T>(data);
-    Node<T> *oldHead = this -> head;
-
-    if (this -> head != nullptr) {
-        newNode -> pointerOnNextElement = oldHead;
-        this -> head = newNode;
-        oldHead -> pointerOnPrevElement = this -> Head;
-        this -> length += 1;
-
-        return;
-    }
-
-    this -> length += 1;
-    this -> head = newNode;
-    this -> tail = newNode;
+void Deque<T>::pushFront(DynamicArray<T> element) {
+    this -> data -> prepend(element);
 }
 
 template<typename T>
-void Deque<T>::pushBack(DynamicArray<T> data) {
-    auto *newNode = new Node<T>(data);
-    Node<T> *oldTail = this -> tail;
-
-    if (this -> head != nullptr) {
-        newNode -> pointerOnPrevElement = oldTail;
-        this -> tail = newNode;
-        oldTail -> pointerOnNextElement = this -> tail;
-        this -> length += 1;
-
-        return;
-    }
-
-    this -> length += 1;
-    this -> head = newNode;
-    this -> tail = newNode;
+void Deque<T>::pushBack(DynamicArray<T> element) {
+    this -> data -> append(element);
 }
 
 template<typename T>
 size_t Deque<T>::getLength() {
-    return this -> length;
+    return this -> data -> length;
 }
 
 template<typename T>
 bool Deque<T>::isEmpty() {
-    return this -> head == nullptr;
+    return this -> data -> head == nullptr;
 }
